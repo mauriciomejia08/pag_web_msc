eff557ddbcdad21c6f19f50e2d21fe5d48a816c9
mauriciomejia08 <maum45@hotmail.com>
2018-11-15 22:09:23 -0500

    barra de navegacion desplegable

--- asset/js/animaciones.js
+++ asset/js/animaciones.js
@@ -1,11 +1,19 @@
 //function(){
 //variables
-var navHorizontal = document.getElementById("var_nav_fija");
 var menu = document.getElementById("menuDesp");
 //funciones
 function desplegarBarra(){
-	alert("Desplegar Barra");
-	navHorizontal.style.width = ()
+		var cont = 1;
+		var tit = document.getElementById("var_nav_fija");
+	
+		function Desplegar_navbar() {
+			tit.style.width = cont+"%";
+			cont++;
+			if(cont>100){
+			clearInterval(despnav);
+			}
+		}
+		var despnav = setInterval(Desplegar_navbar ,9);
 }
 function desplegarMenu(){}
 /*for(i=0; i < enlace.length; i++ ){
