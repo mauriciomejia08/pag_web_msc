a228227b26b4e3d0afc2d532a6894052bfa67a88
mauriciomejia08 <maum45@hotmail.com>
2018-11-23 15:44:44 -0500

    se agrego la barra de navegacion desplegable y el nav y aside

--- asset/js/animaciones.js
+++ asset/js/animaciones.js
@@ -15,64 +15,43 @@ function desplegarBarra(){
 		}
 		var despnav = setInterval(Desplegar_navbar ,9);
 }
-/*function MostrarOcultarBarNav {
-	var cont = 1;
-	var tit = document.getElementById("var_nav_fija");
-
-	function Desplegar_navbar() {
-		tit.style.width = cont+"%";
-		cont++;
-		if(cont>100){
-		clearInterval(despnav);
-		}
-	}
-	var despnav = setInterval(Desplegar_navbar ,9)
-}*/
-/*function MostrarOcultar(r1){
+function MostrarOcultar(r1,aum1,dis1,limaum,limdis){
+	var web1 = document.getElementById("web");
+	var movil1 = document.getElementById("movil");
+	var sol1 = document.getElementById("sol");
 	if(document.getElementById(r1).style.display == 'none'){
+		web1.style.display = 'none';
+		movil1.style.display = 'none';
+		sol1.style.display = 'none';
 		document.getElementById(r1).style.position = 'absolute';
 		document.getElementById(r1).style.display = 'block';
+		aumenta(r1,aum1,limaum);
 	} else {
-		document.getElementById(r1).style.display = 'none';
-	}
-}*/
-function MostrarOcultar(r1){
-	if(document.getElementById(r1).style.display == 'none'){
-		document.getElementById(r1).style.position = 'absolute';
-		document.getElementById(r1).style.display = 'block';
-		aumenta(r1,1);
-		debugger;
-	} else {
-		disminuye(r1,90);
-		document.getElementById(r1).style.display = 'none';
+		disminuye(r1,dis1,limdis);
 	}
 }
-function aumenta(r1,cont){
-	//var cont = 1;
-		alert("aumenta");
-		function aum() {
-		r1.style.width = cont+"%";
-		cont++;
-		console.log(cont);
-		if(cont>90){
-		clearInterval(despnav);
+function aumenta(r1,cont,limaum){
+	function aum() {
+	document.getElementById(r1).style.width = cont+"%";
+	cont++;
+	console.log(cont);
+		if(cont>limaum){
+		clearInterval(stopaum);
 		}
-		}
-		var despnav = setInterval(aum ,9);
-		
 	}
-function disminuye(r1,cont2){
-//		var cont2 = 90;
-	
-			function dism() {
-			r1.style.width = cont2+"%";
-			cont2=cont2-1;
-			if(cont<10){
-			clearInterval(despnav);
+	var stopaum = setInterval(aum ,5);
+	}
+function disminuye(r1,cont2,limdis){
+	function dism() {
+	document.getElementById(r1).style.width = cont2+"%";
+	cont2=cont2-1;
+	console.log(cont2);
+		if(cont2<limdis){
+			clearInterval(stopdism);
+			document.getElementById(r1).style.display = 'none';
 			}
-			}
-			var despnav = setInterval(dism ,9);
+	}
+	var stopdism = setInterval(dism ,5);
 }
 //eventos
-
 //}());
