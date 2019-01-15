fd414794dc223407c5e5ca561e5a991f5ac10675
mauriciomejia08 <maum45@hotmail.com>
2019-01-15 17:26:45 -0500

    se agrego el css y javascript de los botones de avance en el simulador

--- asset/js/animaciones.js
+++ asset/js/animaciones.js
@@ -71,3 +71,67 @@ function faq2vy(v){
 	document.getElementById(dropbtn).style.display = 'none';
 }*/
 //eventos
+	// Array con todas las imágenes que deseamos que se vayan cambiando a
+	// cada clic
+	var imagenes=Array("../asset/img/movil/faqm1/1.jpg","../asset/img/movil/faqm1/2.jpg","../asset/img/movil/faqm1/3.jpg","../asset/img/movil/faqm1/4.jpg","../asset/img/movil/faqm1/5.jpg",
+	"../asset/img/movil/faqm1/6.jpg",
+	"../asset/img/movil/faqm1/7.jpg",
+	"../asset/img/movil/faqm1/8.jpg",
+	"../asset/img/movil/faqm1/9.jpg",
+	"../asset/img/movil/faqm1/10.jpg",
+	"../asset/img/movil/faqm1/11.jpg",
+	"../asset/img/movil/faqm1/12.jpg",
+	"../asset/img/movil/faqm1/13.jpg",
+	"../asset/img/movil/faqm1/14.jpg",
+	"../asset/img/movil/faqm1/15.jpg",
+	"../asset/img/movil/faqm1/16.jpg",
+	"../asset/img/movil/faqm1/17.jpg",
+	"../asset/img/movil/faqm1/18.jpg",
+	"../asset/img/movil/faqm1/19.jpg",
+	"../asset/img/movil/faqm1/20.jpg",
+	"../asset/img/movil/faqm1/21.jpg",
+	"../asset/img/movil/faqm1/22.jpg",
+	"../asset/img/movil/faqm1/23.jpg",
+	"../asset/img/movil/faqm1/24.jpg",
+	"../asset/img/movil/faqm1/25.jpg");
+	var imagenVisible=0;
+ 
+	// Función que cambia la imagen actual por la siguiente
+	function cambiaLeft(img)
+	{
+		imagenVisible--;
+		if(imagenVisible<0)
+		{
+			imagenVisible=24;
+		}
+		document.getElementById("imgMockUp").src=imagenes[imagenVisible];
+		cargarSiguienteImagen();
+	}
+	function cambiaRight(img)
+	{
+		imagenVisible++;
+		if(imagenVisible>=imagenes.length)
+		{
+			imagenVisible=0;
+		}
+		document.getElementById("imgMockUp").src=imagenes[imagenVisible];
+		cargarSiguienteImagen();
+	}
+ 
+	/**
+	 * Esta función carga la siguiente imagen para no tener que esperar su carga
+	 * en el momento de mostrarla.
+	 */
+	function cargarSiguienteImagen()
+	{
+		if((imagenVisible+1)<imagenes.length)
+		{
+			console.log(imagenVisible+1);
+			var imgTmp=new Image();
+			imgTmp.src=imagenes[imagenVisible+1];
+		}
+	}
+ 
+	window.onload=function() {
+		cargarSiguienteImagen();
+	}
